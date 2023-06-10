de12fc2f8b93032d03177d4345775f700b06749b
Alisher <alisher@supertokens.com>
2023-06-10 11:58:22 +0600

    schemas set up & register/login routes created

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DBService } from './db.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(DBService);
+  await prismaService.enableShutdownHooks(app);
+  // Add request body validation middleware
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(4000);
 }
 bootstrap();
