db9ef693ceb7f227abc9318c8603ac12212f76f0
Alisher <alisher@supertokens.com>
2023-06-13 20:35:02 +0600

    echo node version

--- aws/scripts/before_install.sh
+++ aws/scripts/before_install.sh
@@ -4,7 +4,8 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-
+node -v
+npm -v
 #create our working directory if it doesnt exist
 DIR="/home/projects/expense-tracker"
 if [ -d "$DIR" ]; then
