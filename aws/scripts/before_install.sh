b0684ff629e02c0c5761c11fa1b6d2123ec87478
Alisher <alisher@supertokens.com>
2023-06-14 00:09:28 +0600

    test error

--- aws/scripts/before_install.sh
+++ aws/scripts/before_install.sh
@@ -6,6 +6,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 node -v
 npm -v
+exit 125
 #create our working directory if it doesnt exist
 DIR="/home/projects/expense-tracker"
 if [ -d "$DIR" ]; then
